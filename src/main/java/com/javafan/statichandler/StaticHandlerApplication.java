a6f911b2962a5dbad7a870dc29553179fb98c6ca
fanzq <1057600474@qq.com>
2023-10-01 20:35:02 +0800

    init project

---
+++ src/main/java/com/javafan/statichandler/StaticHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.javafan.statichandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticHandlerApplication.class, args);
+    }
+
+}
